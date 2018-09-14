7469011397bd15416f453a0f7d1f19b12170e5f3
propsylang <propsylang@gmail.com>
2018-09-14 22:04:17 +0900

    Update Adjacent_List_Dijkstra_O(ElogV).cpp

--- graph_algorithms/Dijkstra/Adjacent_List_Dijkstra_O(ElogV).cpp
+++ graph_algorithms/Dijkstra/Adjacent_List_Dijkstra_O(ElogV).cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include <algorithm>
+#include <functional>
 #include <cmath>
 #include <vector>
 #include <map>
 #include <set>
 #include <string>
-#include<bitset>
+#include <bitset>
+#include <queue>
 
 using namespace std;
 #define loop(i,n) for(int i = 0; i < (n); ++i)
@@ -13,18 +15,18 @@ using namespace std;
 #define all(v) v.begin(),v.end()
 #define show(v) for(int i = 0; i < v.size(); ++i){cout<<i<<": "<<v[i]<<" "<<endl;}
 #define enld endl
-#define INF 9999999999999
+#define INF 999999999
 //----------------------------------------------------
 struct edge { int to, cost; };//辺が伸びている頂点、その頂点までのコスト
 typedef pair<int, int> P;//pair<最短距離,頂点番号>
-const int MAX_V;
-int V;//頂点数
-vector<vector<edge>> G(MAX_V, vector<edge>());//隣接リスト
-vector<int> d(MAX_V, INF)//頂点sからの最短距離
-void AddEdge(int u, int v, cost)
+int v;//頂点数
+	  //vector<edge> G[100010];//隣接リスト
+vector<vector<edge>> G(100010, vector<edge>());
+vector<int> d(100010, INF);//頂点sからの最短距離
+void AddEdge(int u, int v, int cost)//u->vのコストがcost
 {
-	G[u].push_back(edge{ v,cost });
-	G[v].push_back(edge{ u,cost });
+	edge tmp = { v,cost };
+	G[u].push_back(tmp);
 }
 void Adjacent_List_Dijkstra(int s)
 {
@@ -53,6 +55,12 @@ int main()
 {
 	cin.tie(0);
 	ios::sync_with_stdio(false);
-
+	int e, r; cin >> v >> e >> r;//v=頂点数,e=辺数,r=始点
+	loop(i, e)
+	{
+		int s, t, d; cin >> s >> t >> d;//s->tのコストがd
+		AddEdge(s, t, d);
+	}
+	Adjacent_List_Dijkstra(r);
 	return 0;
 }
